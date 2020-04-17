f69f1216f45dce6fbe8f5b3355b06ec1bbe99e27
Robin <wegner.robin@yahoo.de>
2020-04-17 23:48:36 +0200

    added attackManager and basic spell

---
+++ src/attacks/CustomAttack.gd
@@ -0,0 +1,10 @@
+extends "res://src/attacks/BasicAttack.gd"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
