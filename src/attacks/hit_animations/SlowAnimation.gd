fd302f2657f8d8c875e0e3b2309e04d32ce3bfae
Robin <wegner.robin@yahoo.de>
2020-04-19 00:28:34 +0200

    renaming and call slow

--- src/attacks/hit_animations/SlowAnimation.gd
+++ src/attacks/hit_animations/SlowAnimation.gd
@@ -4,21 +4,29 @@ export var explosion_duration: float = 0.5
 
 var _explosion_damage: float
 var _timer: Timer
+var _slow_factor: float
+var _slow_duration_sec: float
 
-func setup(position: Vector2, radius: float, damage: float):
+func setup(position: Vector2, radius: float, damage: float, slow_factor: float, slow_duration_sec: float):
 	global_position = position
 	_explosion_damage = damage
+	_slow_factor = slow_factor
+	_slow_duration_sec = slow_duration_sec
+	
 	var collision_shape = get_node("Area2D/CollisionShape2D").get_shape()
 	collision_shape.set_radius(radius)
-	get_node("Sprite").set_scale(Vector2(radius, radius))
+	get_node("Sprite").set_scale(Vector2(radius, radius))	
+	_set_timeout()
+
+func _ready():
+	pass
+	
+func _set_timeout():
 	_timer = Timer.new()
 	_timer.set_wait_time(explosion_duration)
 	self.add_child(_timer)
 	_timer.connect("timeout", self, "_on_timer_timeout")
 	_timer.start()
-
-func _ready():
-	pass
 
 #func _process(delta):
 #	pass
@@ -33,3 +41,5 @@ func _physics_process(delta):
 		for body in overlaps:
 			if (body.has_method("inflict_damage")):
 				body.inflict_damage(_explosion_damage)
+			if (body.has_method("apply_slow")):
+				body.apply_slow(_slow_factor, _slow_duration_sec)
