00b6e4c2df7206f5b924ebc0cff14cf4629f77d1
Robin <wegner.robin@yahoo.de>
2020-04-18 18:47:28 +0200

    make stun animation despawn and make damage

--- src/attacks/IslandAttack.gd
+++ src/attacks/IslandAttack.gd
@@ -2,6 +2,7 @@ extends "res://src/attacks/BasicAttack.gd"
 
 export var stun_duration_sec = 0.5
 export var stun_radius = 3.0
+export var explosion_damage = 2.0
 
 var _stun_animation_scene = preload("res://src/attacks/hit_animations/StunAnimation.tscn")
 
@@ -15,11 +16,9 @@ func _ready():
 #	pass
 
 func process_hit(collider: KinematicBody2D):
-	if (collider.has_method("inflict_damage")):
-		print("hit")
-		collider.inflict_damage(damage_intensity)
 	var instance = _stun_animation_scene.instance()
-	instance.setup(stun_radius)
+	$"/root/Main".add_child(instance)
+	instance.setup(collider.global_position, stun_radius, explosion_damage)
 	.process_hit(collider)
 
 
--- src/attacks/hit_animations/StunAnimation.gd
+++ src/attacks/hit_animations/StunAnimation.gd
@@ -1,28 +1,34 @@
 extends Node2D
 
+var _explosion_damage: float
 var _timer: Timer
 
-func setup(radius: float):
+func setup(position: Vector2, radius: float, damage: float):
+	global_position = position
+	_explosion_damage = damage
 	var collision_shape = get_node("Area2D/CollisionShape2D").get_shape()
-	collision_shape.set_radius(collision_shape.get_radius() * radius)
+	var new_radius = collision_shape.get_radius() * radius
+	collision_shape.set_radius(new_radius)
+	get_node("Sprite").set_scale(Vector2(new_radius, new_radius))
 	_timer = Timer.new()
-	add_child(_timer)
-	_timer.connect("timeout", self, "_on_Timer_timeout")
 	_timer.set_wait_time(1.0)
+	self.add_child(_timer)
+	_timer.connect("timeout", self, "_on_Timer_timeout")	
 	_timer.start()
-	queue_free()
 
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 #func _process(delta):
 #	pass
 
+func _on_Timer_timeout():
+	_timer.queue_free()
+	queue_free()
+
 func _physics_process(delta):
-	print("update")
 	var overlaps = get_node("Area2D").get_overlapping_bodies()
 	if (overlaps.size() > 0):
 		for body in overlaps:
-			_timer.stop()
-			body.queue_free()
+			if (body.has_method("inflict_damage")):
+				body.inflict_damage(_explosion_damage)
