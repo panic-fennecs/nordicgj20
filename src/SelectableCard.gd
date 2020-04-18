e703e978653847019ffe77fd701bde319cc0bc7c
Philipp <philippboensch@gmail.com>
2020-04-19 01:52:51 +0200

    implemented card elimination screen

--- src/SelectableCard.gd
+++ src/SelectableCard.gd
@@ -11,6 +11,11 @@ func setup(card_type: String, already_banned: bool) -> void:
 		$Sprite.modulate = Color("2a2929")
 		$EliminateSprite.visible = eliminated
 	
+func unselect():
+	if _banned: return
+	eliminated = false
+	$EliminateSprite.visible = eliminated
+
 func _on_SelectableCard_input_event(viewport, event, shape_idx) -> void:
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
