8f320a6895e1fbdc7042487ac72ac62856f21434
memoryleak47 <memoryleak47@protonmail.ch>
2020-04-18 14:20:25 +0200

    implemented dash

--- src/Player.gd
+++ src/Player.gd
@@ -4,10 +4,15 @@ const SPEED: float = 50.0
 const MAX_SPEED: float = 200.0
 const DRAG: float = 0.8
 const MAX_HEALTH: float = 1.0
+const DASH_SPEED: float = 500.0
+const DASH_RANGE: float = 1000.0
 
 var _velocity: Vector2 = Vector2.ZERO
 var health: float = MAX_HEALTH
 signal health_changed(new_health)
+
+var dash_dist = null
+var dash_direction = null
 
 func _ready():
 	emit_signal("health_changed", health)
@@ -29,12 +34,19 @@ func _physics_process(delta: float) -> void:
 	
 	direction = direction.normalized()
 	
-	_velocity += direction * SPEED
-	if _velocity.length() > MAX_SPEED:
-		_velocity = _velocity.normalized() * MAX_SPEED
-		
-	_velocity = move_and_slide(_velocity)
-	_velocity *= DRAG
+	if dash_dist:
+		_velocity = dash_direction.normalized() * DASH_SPEED
+		dash_dist -= DASH_SPEED
+		if dash_dist <= 0:
+			dash_dist = null
+			dash_direction = null
+		_velocity = move_and_slide(_velocity)
+	else:
+		_velocity += direction * SPEED
+		if _velocity.length() > MAX_SPEED:
+			_velocity = _velocity.normalized() * MAX_SPEED
+		_velocity = move_and_slide(_velocity)
+		_velocity *= DRAG
 	
 func set_health(new_health):
 	health = clamp(new_health, 0, MAX_HEALTH)
@@ -50,3 +62,7 @@ func heal(h):
 func _try_loose():
 	if health < 0:
 		pass
+
+func dash():
+	dash_direction = (to_global(get_global_mouse_position()) - global_position).normalized()
+	dash_dist = DASH_RANGE
