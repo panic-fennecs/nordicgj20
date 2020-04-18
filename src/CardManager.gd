3e6cebd34b1685c5c60d5863d6c4b91ed1327c4c
memoryleak47 <memoryleak47@protonmail.ch>
2020-04-18 19:03:08 +0200

    implemented card cooldown

--- src/CardManager.gd
+++ src/CardManager.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+const COOLDOWN = 2.0
+
 var visible_cards = []
+var cooldown = 0.0
 
 func _ready():
 	for i in range(3):
@@ -8,9 +11,14 @@ func _ready():
 
 func _process(delta):
 	update_sprites()
+	cooldown -= delta
+	if cooldown < 0:
+		cooldown = 0
 
 func throw_card(direction):
-	$"/root/Main/AttackManager".activate_card(consume_card(), direction)
+	if cooldown == 0:
+		$"/root/Main/AttackManager".activate_card(consume_card(), direction)
+		cooldown = COOLDOWN
 
 func consume_card():
 	var t = visible_cards[0]
@@ -30,6 +38,9 @@ func get_sprite(c):
 	s.texture = get_image(c)
 	return s
 
+func cardsize():
+	return get_sprite(visible_cards[0]).get_rect().size
+
 func update_sprites():
 	var offset = 3
 
@@ -38,9 +49,23 @@ func update_sprites():
 		cl.remove_child(c)
 		c.queue_free()
 
+	var size = cardsize()
+
+	var s = get_sprite("card_background")
+	s.position.x = size.x / 2 + offset
+	s.position.y = get_viewport_rect().size.y - size.y / 2 - offset
+	# s.position -= (s.get_rect().size - size) / 2
+	cl.add_child(s)
+
+	var realsize = Vector2(76.0, 114.0)
+	var cs = ColorRect.new()
+	cs.color = Color.black
+	cs.set_position(s.position - realsize / 2.0)
+	cs.set_size(Vector2(realsize.x, realsize.y * (cooldown / COOLDOWN)))
+	cl.add_child(cs)
+
 	for i in range(3):
-		var s = get_sprite(visible_cards[i])
-		var size = s.get_rect().size
-		s.position.x = size.x / 2 + offset + i * (offset + size.x)
-		s.position.y = get_viewport_rect().size.y - size.y / 2 - offset
-		cl.add_child(s)
+		var s2 = get_sprite(visible_cards[i])
+		s2.position.x = size.x / 2 + offset + i * (offset + size.x)
+		s2.position.y = get_viewport_rect().size.y - size.y / 2 - offset
+		cl.add_child(s2)
