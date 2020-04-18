69cefb52ee74c647e78436045143e9fc7391079a
Robin <wegner.robin@yahoo.de>
2020-04-19 00:29:16 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/EnemyRuneclawBear.gd
+++ src/EnemyRuneclawBear.gd
@@ -6,7 +6,7 @@ const ATTACK_COOLDOWN = 4.0
 var cooldown = 0.0
 
 func _ready():
-	self.do_task(PatrolTask.new())
+	self.do_task(PatrolTask.new(null, 40 * _slow))
 
 func _get_default_task():
 	return PatrolTask.new()
@@ -15,11 +15,13 @@ func _physics_process(delta):
 	var player = $"/root/Main/YSort/Player"
 	if cooldown <= 0:
 		if player.position.distance_squared_to(self.position) < ATTACK_TRIGGER_RANGE*ATTACK_TRIGGER_RANGE:
-			var attack_task = AttackTask.new($"/root/Main/YSort/Player", 450.0, 50.0, ATTACK_TRIGGER_RANGE+50.0)
+			var attack_task = AttackTask.new($"/root/Main/YSort/Player", 450.0 * _slow, 50.0, ATTACK_TRIGGER_RANGE+50.0)
 			self.do_task(ListenTask.new(0.5, attack_task))
 			cooldown = ATTACK_COOLDOWN
 	else:
 		cooldown -= delta
+		
+	print(_slow)
 
 func target_point_found():
 	$"/root/Main/YSort/Player".inflict_damage(0.3)
