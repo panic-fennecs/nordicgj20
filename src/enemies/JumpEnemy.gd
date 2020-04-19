1aafa9325dd374b69b77c181e723915c09bb6707
ichinii <sven.salt@yahoo.de>
2020-04-19 13:04:04 +0000

    even further JumpEnemy random target position offset

--- src/enemies/JumpEnemy.gd
+++ src/enemies/JumpEnemy.gd
@@ -36,8 +36,8 @@ func _jump() -> void:
 	if _state_changed:
 		_state_changed = false
 		_target = $"/root/Main/YSort/Player".position
-		_target.x += randf() * 200 - 100
-		_target.y += randf() * 200 - 100
+		_target.x += randf() * 320 - 160
+		_target.y += randf() * 320 - 160
 		var v = _target - position
 		var l = v.length()
 		_target = position + v.normalized() * min(300, l)
